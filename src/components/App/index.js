16f63b7127cf1f1ce5d44e0e1c5fbdc400a6f6ab
Reuben Berghan <reuben.berghan@gmail.com>
2018-07-07 11:49:36 +1200

    First pass at board squares

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import styled from 'styled-components'
+import { times } from 'ramda'
 
-import { Board } from '..'
+import { Board, Square } from '..'
 
 const StyledApp = styled.div`
   display: grid;
@@ -10,25 +11,21 @@ const StyledApp = styled.div`
   height: 100vh;
   margin: 0;
   padding: 0;
-  width: 100vh;
+  width: 100vw;
 `
+function makeSquares () {
+  return times(
+    idx => <Square key={idx} index={idx} player={idx % 2 === 0 ? 'x' : 'o'} />,
+    9
+  )
+}
 
 StyledApp.displayName = 'StyledApp'
 
 export default function App () {
   return (
     <StyledApp>
-      <Board>
-        <div>0</div>
-        <div>1</div>
-        <div>2</div>
-        <div>3</div>
-        <div>4</div>
-        <div>5</div>
-        <div>6</div>
-        <div>7</div>
-        <div>8</div>
-      </Board>
+      <Board>{makeSquares()}</Board>
     </StyledApp>
   )
 }
