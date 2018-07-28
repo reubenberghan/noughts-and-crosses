d14513f8f4086e66f410f89b64a1e4c489c4c6fe
Reuben Berghan <reuben.berghan@gmail.com>
2018-07-28 16:22:06 +1200

    Mark the winning squares

--- src/containers/Square/index.js
+++ src/containers/Square/index.js
@@ -1,15 +1,19 @@
 import { connect } from 'react-redux'
+import { contains } from 'ramda'
 
 import Square from '../../components/Square'
-import { getMoves, squareClicked } from '../../state'
+import { getMoves, getWinningSquares, squareClicked } from '../../state'
 import { getPlayer } from '../../utilities'
 
 function mapStateToProps (state, { index }) {
   const moves = getMoves(state)
+  const winners = getWinningSquares(state)
+  const gameIsOver = !!winners
+  const player = getPlayer(index, moves)
 
-  return {
-    player: getPlayer(index, moves)
-  }
+  return gameIsOver
+    ? { player, isWinningSquare: contains(index, winners) }
+    : { player }
 }
 
 function mapDispatchToProps (dispatch, { index }) {
